18d52e8d166be71d395b3d11d6c049c715b82c5d
Rachwan <rachwan.harb2023@gmail.com>
2023-09-26 21:51:52 +0300

    Step 3

--- index.js
+++ index.js
@@ -6,4 +6,20 @@ app.get('/', (request, response) => {
     response.send('ok')
 });
 
-app.listen(port, () => console.log(`The server is lisining on port: ${port}`))
+app.listen(port, () => console.log(`The server is lisining on port: ${port}`));
+
+app.get('test', (request, response) => {
+    response.json({
+        status: 200,
+        message: 'ok',
+    })
+})
+
+app.get('time', (request, response) => {
+    const date = new Date();
+    const time = `${date.getHours()}:${date.getMinutes}`;
+    response.json({
+        status: 200,
+        message: time,
+    })
+})
